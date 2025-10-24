9f86be797b1d387ec839e45aafa3d4426dc7d2d9
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2025-10-24 18:06:35 +0200

    chore: import architectury multiplatform template

---
+++ common/src/main/java/tech/alexnijjar/golemoverhaul/ExampleMod.java
@@ -0,0 +1,9 @@
+package tech.alexnijjar.golemoverhaul;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "golemoverhaul";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ forge/src/main/java/tech/alexnijjar/golemoverhaul/forge/ExampleModForge.java
@@ -0,0 +1,18 @@
+package tech.alexnijjar.golemoverhaul.forge;
+
+import dev.architectury.platform.forge.EventBuses;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import tech.alexnijjar.golemoverhaul.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModForge {
+    public ExampleModForge() {
+        // Submit our event bus to let Architectury API register our content on the right time.
+        EventBuses.registerModEventBus(ExampleMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
