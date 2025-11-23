79dbe258501a19c14c783edad74d0f5342d4acf0
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2025-11-23 15:59:31 +0100

    fix: make entrypoint compatible with neoforge

--- forge/src/main/java/tech/alexnijjar/golemoverhaul/forge/GolemOverhaulForge.java
+++ forge/src/main/java/tech/alexnijjar/golemoverhaul/forge/GolemOverhaulForge.java
@@ -1,26 +1,24 @@
 package tech.alexnijjar.golemoverhaul.forge;
 
-import com.teamresourceful.resourcefulconfig.client.ConfigScreen;
 import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLLoader;
 import tech.alexnijjar.golemoverhaul.GolemOverhaul;
-import tech.alexnijjar.golemoverhaul.common.config.GolemOverhaulConfig;
 import tech.alexnijjar.golemoverhaul.forge.client.GolemOverhaulForgeClient;
 
+@SuppressWarnings("removal")
 @Mod(GolemOverhaul.MOD_ID)
 public final class GolemOverhaulForge {
 
-    public GolemOverhaulForge(FMLJavaModLoadingContext context) {
-        EventBuses.registerModEventBus(GolemOverhaul.MOD_ID, context.getModEventBus());
+    public GolemOverhaulForge() {
+        EventBuses.registerModEventBus(GolemOverhaul.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
 
         GolemOverhaul.init();
 
         if (FMLLoader.getDist() == Dist.CLIENT) {
-            GolemOverhaulForgeClient.init(context);
+            GolemOverhaulForgeClient.init();
         }
     }
 }
--- forge/src/main/java/tech/alexnijjar/golemoverhaul/forge/client/GolemOverhaulForgeClient.java
+++ forge/src/main/java/tech/alexnijjar/golemoverhaul/forge/client/GolemOverhaulForgeClient.java
@@ -2,14 +2,17 @@ package tech.alexnijjar.golemoverhaul.forge.client;
 
 import com.teamresourceful.resourcefulconfig.client.ConfigScreen;
 import net.minecraftforge.client.ConfigScreenHandler;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.ModLoadingContext;
 import tech.alexnijjar.golemoverhaul.GolemOverhaul;
 import tech.alexnijjar.golemoverhaul.common.config.GolemOverhaulConfig;
 
+@SuppressWarnings("removal")
 public class GolemOverhaulForgeClient {
 
-    public static void init(FMLJavaModLoadingContext context) {
-        context.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
+    public static void init() {
+        var container = ModLoadingContext.get().getActiveContainer();
+
+        container.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
                 () -> new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> {
                     var config = GolemOverhaul.CONFIGURATOR.getConfig(GolemOverhaulConfig.class);
                     if (config == null) return null;
