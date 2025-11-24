67e088a671723d407e6259bdb7f4308961f32438
Kevin <70778888+kvnmtz@users.noreply.github.com>
2025-11-24 11:12:31 +0100

    fix: use mixin to spawn additional village golems (#21)
    * fix: use mixin to spawn additional village golems
    
    This is an alternative approach to spawning golems in villages alongside the standard iron golem. By using a mixin we don't have to "destructively" overwrite a vanilla structure, increasing compatibility.
    
    * fix: respect config values when spawning village golems
    
    * fix: replicate vanilla spawning behaviour
    
    * fix: use prefix and add Unique annotations

---
+++ src/main/java/tech/alexnijjar/golemoverhaul/mixins/common/SinglePoolElementMixin.java
@@ -0,0 +1,102 @@
+package tech.alexnijjar.golemoverhaul.mixins.common;
+
+import com.mojang.datafixers.util.Either;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.level.StructureManager;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.level.levelgen.structure.pools.SinglePoolElement;
+import net.minecraft.world.level.levelgen.structure.templatesystem.LiquidSettings;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import tech.alexnijjar.golemoverhaul.common.entities.golems.BarrelGolem;
+import tech.alexnijjar.golemoverhaul.common.entities.golems.HayGolem;
+import tech.alexnijjar.golemoverhaul.common.entities.golems.base.BaseGolem;
+import tech.alexnijjar.golemoverhaul.common.registry.ModEntityTypes;
+
+@Mixin(SinglePoolElement.class)
+public abstract class SinglePoolElementMixin {
+
+    @Unique
+    private static final ResourceLocation golemoverhaul$IRON_GOLEM_STRUCTURE =
+            ResourceLocation.withDefaultNamespace("village/common/iron_golem");
+
+    @Shadow
+    @Final
+    protected Either<ResourceLocation, StructureTemplate> template;
+
+    @Shadow
+    protected abstract StructurePlaceSettings getSettings(Rotation rotation, BoundingBox boundingBox,
+                                                          LiquidSettings liquidSettings, boolean offset);
+
+    @Inject(
+            method = "place(Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplateManager;" +
+                    "Lnet/minecraft/world/level/WorldGenLevel;Lnet/minecraft/world/level/StructureManager;" +
+                    "Lnet/minecraft/world/level/chunk/ChunkGenerator;Lnet/minecraft/core/BlockPos;" +
+                    "Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/Rotation;" +
+                    "Lnet/minecraft/world/level/levelgen/structure/BoundingBox;" +
+                    "Lnet/minecraft/util/RandomSource;" +
+                    "Lnet/minecraft/world/level/levelgen/structure/templatesystem/LiquidSettings;Z)Z",
+            at = @At("RETURN")
+    )
+    private void golemoverhaul$place(StructureTemplateManager structureTemplateManager, WorldGenLevel worldGenLevel,
+                                     StructureManager structureManager, ChunkGenerator generator, BlockPos offset,
+                                     BlockPos pos, Rotation rotation, BoundingBox box, RandomSource random,
+                                     LiquidSettings liquidSettings, boolean keepJigsaws,
+                                     CallbackInfoReturnable<Boolean> cir) {
+        if (!cir.getReturnValue()) return;
+        if (worldGenLevel.isClientSide()) return;
+
+        this.template.left().ifPresent(templateLocation -> {
+            if (!templateLocation.equals(golemoverhaul$IRON_GOLEM_STRUCTURE)) return;
+
+            var settings = this.getSettings(rotation, box, liquidSettings, keepJigsaws);
+
+            var barrelGolemEntityType = ModEntityTypes.BARREL_GOLEM.get();
+            if (BarrelGolem.checkMobSpawnRules(barrelGolemEntityType, worldGenLevel, MobSpawnType.SPAWNER, offset,
+                    worldGenLevel.getRandom())) {
+                golemoverhaul$spawnAdditionalGolem(barrelGolemEntityType, worldGenLevel, settings, offset);
+            }
+
+            var hayGolemEntityType = ModEntityTypes.HAY_GOLEM.get();
+            if (HayGolem.checkMobSpawnRules(hayGolemEntityType, worldGenLevel, MobSpawnType.SPAWNER, offset,
+                    worldGenLevel.getRandom())) {
+                golemoverhaul$spawnAdditionalGolem(hayGolemEntityType, worldGenLevel, settings, offset);
+            }
+        });
+    }
+
+    @Unique
+    private <T extends BaseGolem> void golemoverhaul$spawnAdditionalGolem(EntityType<T> entityType, WorldGenLevel worldGenLevel,
+                                                                          StructurePlaceSettings settings, BlockPos offset) {
+        var golem = entityType.create(worldGenLevel.getLevel());
+        if (golem == null) return;
+
+        var spawnPos = new Vec3(offset.getX() + 0.5, offset.getY() + 1, offset.getZ() + 0.5);
+
+        float yRot = golem.rotate(settings.getRotation());
+        yRot += golem.mirror(settings.getMirror()) - golem.getYRot();
+        golem.moveTo(spawnPos.x, spawnPos.y, spawnPos.z, yRot, golem.getXRot());
+        if (settings.shouldFinalizeEntities()) {
+            golem.finalizeSpawn(worldGenLevel,
+                    worldGenLevel.getCurrentDifficultyAt(BlockPos.containing(spawnPos)), MobSpawnType.STRUCTURE, null);
+        }
+
+        worldGenLevel.addFreshEntityWithPassengers(golem);
+    }
+}
